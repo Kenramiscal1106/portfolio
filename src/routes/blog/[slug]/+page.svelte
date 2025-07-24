60d741c100f70f5901a948a381a53a60607fc62e
Ken Ramiscal <code.ramiscalken@gmail.com>
2025-07-24 21:46:18 +0800

    add page for blog page

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import Label from '$lib/components/Label.svelte';
+
+	// import type { PageData } from './$types';
+	const { title, description, date, readTime, imageURL, imageAlt } = {
+		title: 'My Coding Journey',
+		description:
+			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut sint fugit, dicta a aliquid ',
+		date: 'July 20, 2022',
+		readTime: '9 min',
+		imageURL: 'string',
+		imageAlt: 'A mountain climber'
+	};
+</script>
+
+<div class="mb-3">
+	<h1 class="mb-1 text-2xl font-bold">{title}</h1>
+	<div class="flex gap-3 text-sm">
+		<Label>
+			<svg
+				class="aspect-square h-6"
+				viewBox="0 0 24 24"
+				stroke="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M6.75 2.25232C7.16421 2.25232 7.5 2.58811 7.5 3.00232V4.50232H16.5V3.00232C16.5 2.58811 16.8358 2.25232 17.25 2.25232C17.6642 2.25232 18 2.58811 18 3.00232V4.50232H18.75C20.4069 4.50232 21.75 5.84547 21.75 7.50232V18.7523C21.75 20.4092 20.4069 21.7523 18.75 21.7523H5.25C3.59315 21.7523 2.25 20.4092 2.25 18.7523V7.50232C2.25 5.84547 3.59315 4.50232 5.25 4.50232H6V3.00232C6 2.58811 6.33579 2.25232 6.75 2.25232ZM20.25 11.2523C20.25 10.4239 19.5784 9.75232 18.75 9.75232H5.25C4.42157 9.75232 3.75 10.4239 3.75 11.2523V18.7523C3.75 19.5807 4.42157 20.2523 5.25 20.2523H18.75C19.5784 20.2523 20.25 19.5807 20.25 18.7523V11.2523Z"
+				/>
+			</svg>
+
+			<span>{date}</span>
+		</Label>
+		<Label>
+			<svg class="aspect-square h-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					stroke="none"
+					d="M10.3939 2.14963C5.99723 2.14963 2.43304 5.71382 2.43304 10.1105C2.43304 14.5071 5.99723 18.0713 10.3939 18.0713C14.7905 18.0713 18.3547 14.5071 18.3547 10.1105C18.3547 5.71382 14.7905 2.14963 10.3939 2.14963ZM11.0063 5.21149C11.0063 4.87329 10.7321 4.59912 10.3939 4.59912C10.0557 4.59912 9.78151 4.87329 9.78151 5.21149V10.1105C9.78151 10.4487 10.0557 10.7228 10.3939 10.7228H14.0681C14.4063 10.7228 14.6805 10.4487 14.6805 10.1105C14.6805 9.77227 14.4063 9.4981 14.0681 9.4981H11.0063V5.21149Z"
+				/>
+			</svg>
+
+			<span>{readTime}</span>
+		</Label>
+	</div>
+</div>
+<div class="from-neutrals-600 to-neutrals-300 rounded-xl bg-gradient-to-bl">
+	<img src={imageURL} alt={imageAlt} class="aspect-video w-full" />
+</div>
