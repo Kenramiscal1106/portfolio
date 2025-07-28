c9972656097eadf8d31b3f562ae023a3ce6d665b
Ken Ramiscal <code.ramiscalken@gmail.com>
2025-07-28 09:31:00 +0800

    add Post type assertion

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,9 +1,10 @@
+import type { Post } from '$lib';
 import type { PageLoad } from './$types';
 
 export const load = (async ({params}) => {
     const page = await import(`../../../blogs/${params.slug}.svx`);
     return {
         content: page.default,
-        meta: page.metadata
+        meta: page.metadata as Post
     };
 }) satisfies PageLoad;
