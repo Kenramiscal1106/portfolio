3a93cfcf92f0ea2e8c00b705e34a3dc95a6baf03
Ken Ramiscal <code.ramiscalken@gmail.com>
2025-07-28 09:31:19 +0800

    update styling

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,16 +1,6 @@
 <script lang="ts">
 	import BlogCard from '$lib/components/BlogCard.svelte';
 	import type { PageData } from './$types';
-	const blogData = {
-		title: 'My Coding Journey',
-		description:
-			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut sint fugit, dicta a aliquid ',
-		date: 'July 20, 2022',
-		readTime: '9 min',
-		imageURL: 'string',
-		imageAlt: 'A mountain climber',
-		slug: 'my-coding-journey'
-	};
 	export let data: PageData;
 </script>
 
@@ -19,7 +9,6 @@
 </svelte:head>
 
 <div class="flex flex-col gap-6">
-	<BlogCard {...blogData} />
 	<div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
 		{#each data.posts as post}
 			<BlogCard {...post} />
