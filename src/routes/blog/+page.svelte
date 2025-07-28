38dad47819f9454ac2f22fdc19a2ad71b9fff8c5
Ken Ramiscal <code.ramiscalken@gmail.com>
2025-07-28 08:35:05 +0800

    refactor for present md files

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import BlogCard from '$lib/components/BlogCard.svelte';
+	import type { PageData } from './$types';
 	const blogData = {
 		title: 'My Coding Journey',
 		description:
@@ -10,7 +11,7 @@
 		imageAlt: 'A mountain climber',
 		slug: 'my-coding-journey'
 	};
-	// export let data: PageData;
+	export let data: PageData;
 </script>
 
 <svelte:head>
@@ -20,8 +21,8 @@
 <div class="flex flex-col gap-6">
 	<BlogCard {...blogData} />
 	<div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-		{#each new Array(12) as _}
-			<BlogCard {...blogData} />
+		{#each data.posts as post}
+			<BlogCard {...post} />
 		{/each}
 	</div>
 </div>
