3299de7c9bb3dc8cdaad19a601616b1ad26c0a80
Ken Ramiscal <code.ramiscalken@gmail.com>
2025-07-30 00:21:57 +0800

    formatted file and sorted

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,29 +1,30 @@
-import type { Post } from "$lib";
-import type { PageServerLoad } from "./$types";
-
-
+import type { Post } from '$lib';
+import type { PageServerLoad } from './$types';
 
 async function getPosts() {
-    const posts: Post[] = [];
+	const posts: Post[] = [];
 
-    const paths = import.meta.glob('/src/blogs/*.svx', {eager:true});
+	const paths = import.meta.glob('/src/blogs/*.svx', { eager: true });
 	// console.log(paths);
-    Object.keys(paths).forEach(path => {
+	Object.keys(paths).forEach((path) => {
+		const file = paths[path];
+		const slug = path.split('/').at(-1)?.replace('.svx', '');
 
-        const file = paths[path];
-        const slug = path.split('/').at(-1)?.replace('.svx', '');
-    
-        if (file && typeof file === 'object' && 'metadata' in file && slug) {
-            const metadata = file.metadata as Omit<Post, 'slug'>;
-            const post = { ...metadata, slug } satisfies Post;
-            posts.push(post);
-        }
-    })
-    return posts
+		if (file && typeof file === 'object' && 'metadata' in file && slug) {
+			const metadata = file.metadata as Omit<Post, 'slug'>;
+			const post = { ...metadata, slug } satisfies Post;
+			posts.push(post);
+		}
+	});
+	return posts.sort((a, b) => {
+		const postA = Date.parse(a.createdAt);
+		const postB = Date.parse(b.createdAt);
+		return postB - postA;
+	});
 }
 
 export const load: PageServerLoad = async () => {
-  const posts = await getPosts();
-  posts.map
-  return {posts}
+	const posts = await getPosts();
+
+	return { posts };
 };
