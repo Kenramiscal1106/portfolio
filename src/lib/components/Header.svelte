16af50c0cc74386305d2ab43dc4c8f8bf5174863
Ken Ramiscal <code.ramiscalken@gmail.com>
2025-08-01 07:52:27 +0800

    refactor navigation

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,39 +1,10 @@
 <script lang="ts">
 	import { MediaQuery } from 'svelte/reactivity';
-	import Button from './Button.svelte';
-	import { menu } from '$lib/store.svelte';
+	import Nav from './Nav.svelte';
 	const small = new MediaQuery('max-width: 512px');
 	$inspect(small.current);
 </script>
 
 <header class="bg-neutrals-100">
-	<nav class=" mx-auto flex max-w-2xl items-center justify-between px-6 py-3 sm:py-4">
-		<div>
-			<a href="/"><img src="/Logo.svg" alt="logo" height="34" width="124" /></a>
-		</div>
-		<div>
-			<Button
-				type="ghost"
-				onclick={() => {
-					menu.open();
-				}}
-			>
-				Menu
-				<svg
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						fill-rule="evenodd"
-						clip-rule="evenodd"
-						d="M3 6.75C3 6.33579 3.33579 6 3.75 6H20.25C20.6642 6 21 6.33579 21 6.75C21 7.16421 20.6642 7.5 20.25 7.5H3.75C3.33579 7.5 3 7.16421 3 6.75ZM3 12C3 11.5858 3.33579 11.25 3.75 11.25H20.25C20.6642 11.25 21 11.5858 21 12C21 12.4142 20.6642 12.75 20.25 12.75H3.75C3.33579 12.75 3 12.4142 3 12ZM11.25 17.25C11.25 16.8358 11.5858 16.5 12 16.5H20.25C20.6642 16.5 21 16.8358 21 17.25C21 17.6642 20.6642 18 20.25 18H12C11.5858 18 11.25 17.6642 11.25 17.25Z"
-						fill="#0F172A"
-					/>
-				</svg>
-			</Button>
-		</div>
-	</nav>
+	<Nav />
 </header>
--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
+	import { onNavigate } from '$app/navigation';
 	import { menu } from '$lib/store.svelte';
+	import { bounceInOut, sineInOut } from 'svelte/easing';
+	import { fade, fly } from 'svelte/transition';
+	onNavigate(({ to, from }) => {
+		if (to?.route.id !== from?.route.id) {
+			menu.close();
+		}
+	});
 </script>
 
 {#if menu.current}
 	<button
 		aria-label="overlay"
 		class="bg-neutrals-100/35 absolute top-0 left-0 h-dvh w-screen"
+		transition:fade={{ duration: 200 }}
 		onclick={() => {
 			menu.close();
 		}}
 	></button>
 	<div
-		class="bg-neutrals-100 absolute right-0 bottom-0 flex h-1/2 w-screen flex-col p-6 text-center text-lg sm:top-0 sm:w-80"
+		class="bg-neutrals-100 absolute right-0 bottom-0 flex h-dvh w-full max-w-64 flex-col p-6 text-center text-lg sm:top-0 sm:w-80"
+		transition:fly={{ x: '100%', duration: 200, easing: sineInOut, opacity: 1 }}
 	>
 		<a class="py-2" href="/">Home</a>
 		<a class="py-2" href="/blog">Blog</a>
