e29ae7c9c0a56adcfc75ea9fafefc27016cbf5e5
Ken Ramiscal <code.ramiscalken@gmail.com>
2025-08-01 09:04:04 +0800

    add attributes and variant to button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -5,33 +5,44 @@
 	const btnclass = {
 		primary: 'text-neutrals-100 scale-95',
 		neutral: 'bg-neutrals-900 text-neutrals-100 active:scale-95',
+		outline: 'border-neutrals-300 border-2 hover:border-neutrals-400',
 		ghost: 'hover:bg-neutrals-200 active:bg-neutrals-300'
 	};
 	interface Props {
 		children: Snippet;
-		variant?: 'primary' | 'neutral' | 'ghost';
+		variant?: 'primary' | 'neutral' | 'ghost' | 'outline';
 		onclick?: MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
 		href?: string;
 		class?: string;
+		[key: string]: any;
 	}
-	const { children, variant = 'primary', onclick, href, class: className }: Props = $props();
+	const {
+		children,
+		variant = 'primary',
+		onclick,
+		href,
+		class: className,
+		...props
+	}: Props = $props();
 </script>
 
 {#if href}
 	<a
 		{href}
-		class="flex items-center gap-2 rounded-lg p-2 transition-colors duration-100 {btnclass[
+		class="flex items-center justify-center gap-2 rounded-lg p-2 transition-colors duration-100 {btnclass[
 			variant
 		]} {className ?? ''}"
 		{onclick}
+		{...props}
 	>
 		{@render children()}
 	</a>
 {:else}
 	<button
-		class="flex items-center gap-2 rounded-lg p-2 transition-colors duration-100 {btnclass[
+		class="flex items-center justify-center gap-2 rounded-lg p-2 transition-colors duration-100 {btnclass[
 			variant
 		]} {className ?? ''}"
+		{...props}
 		{onclick}
 	>
 		{@render children()}
