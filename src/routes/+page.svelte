be1b6b9e3c4d89acc68474e0eac5cbcf6e0e1106
Ken Ramiscal <code.ramiscalken@gmail.com>
2025-08-01 09:04:42 +0800

    add buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,6 +54,7 @@
 			href="https://linkedin.com"
 			title="LinkedIn Profile"
 			aria-label="LinkedIn Profile"
+			target="_blank"
 			class="hover:fill-neutrals-600 active:fill-neutrals-800"
 			><svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
 				<g>
