04b307650c62cacc9a97a8912ba959f3b265e241
Ken Ramiscal <code.ramiscalken@gmail.com>
2025-08-06 12:49:43 +0800

    restructure project dom tree

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
 	import type { Project } from '$lib';
-	type Props = Project;
-	const { title, description, imageURL, imageAlt, slug }: Props = $props();
+	import Button from './Button.svelte';
+	type Props = Project & { class: string };
+	const { title, description, imageURL, imageAlt, slug, class: className }: Props = $props();
 </script>
 
-<a href="/project/{slug}" class="group">
-	<article
-		class="flex h-full flex-col overflow-hidden rounded-xl transition-all duration-150 hover:-translate-y-1 hover:shadow-lg"
-	>
-		<div class="from-neutrals-600 to-neutrals-300 bg-gradient-to-b">
-			<img src={imageURL} alt={imageAlt} class="aspect-video w-full" />
-		</div>
-		<div class="bg-neutrals-100 flex grow flex-col justify-between gap-4 px-5 py-4">
-			<div class="flex flex-col gap-1">
-				<h1
-					class="group-hover:text-primary-600 text-xl font-bold transition-colors duration-200 active:underline sm:text-2xl sm:active:no-underline lg:text-2xl"
-				>
-					{title}
-				</h1>
+<article
+	class="flex h-full flex-col overflow-hidden rounded-xl transition-all duration-150 {className}"
+>
+	<div class="from-neutrals-600 to-neutrals-300 bg-gradient-to-b">
+		<img src={imageURL} alt={imageAlt} class="aspect-video w-full" />
+	</div>
+	<div class="bg-neutrals-100 flex flex-col justify-between gap-4 px-5 py-4">
+		<div class="flex flex-col gap-1">
+			<h1
+				class="text-xl font-bold transition-colors duration-200 active:underline sm:text-2xl sm:active:no-underline lg:text-2xl"
+			>
+				{title}
+			</h1>
 
-				<p class="line-clamp-3 max-h-[8ch] overflow-hidden text-sm sm:text-base">
-					{description}
-				</p>
+			<p class="line-clamp-3 max-h-[8ch] overflow-hidden text-sm sm:text-base">
+				{description}
+			</p>
+			<div class="mt-5">
+				<Button href="/project/{slug}" variant="neutral">Read More</Button>
 			</div>
 		</div>
-	</article>
-</a>
+	</div>
+</article>
