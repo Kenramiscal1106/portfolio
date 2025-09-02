f1280da380b25dba5887caa8c02adb93ea80b16c
Kenramiscal1106 <code.ramiscalken@gmail.com>
2025-09-02 13:36:18 +0800

    update styling

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 	import type { MouseEventHandler } from 'svelte/elements';
-
 	const btnclass = {
-		primary: 'text-neutrals-100 scale-95',
+		primary: 'text-neutrals-100 scale-95 bg-primary-700',
 		neutral: 'bg-neutrals-900 text-neutrals-100 active:scale-95',
 		outline: 'border-neutrals-300 border-2 hover:border-neutrals-400',
 		ghost: 'hover:bg-neutrals-200 active:bg-neutrals-300'
@@ -33,7 +32,9 @@
 		{href}
 		class="flex items-center justify-center gap-2 rounded-lg {iconOnly
 			? 'p-1.5'
-			: 'px-3 py-1.5'} transition-colors duration-100 {btnclass[variant]} {className ?? ''}"
+			: 'px-3 py-1.5 sm:py-2'} w-max transition-colors duration-100 {btnclass[
+			variant
+		]} {className ?? ''}"
 		{onclick}
 		{...props}
 	>
@@ -43,7 +44,9 @@
 	<button
 		class="flex items-center justify-center gap-2 rounded-lg {iconOnly
 			? 'p-1.5'
-			: 'px-3 py-1.5'} transition-colors duration-100 {btnclass[variant]} {className ?? ''}"
+			: 'px-3 py-1.5 sm:px-4 sm:py-2'} transition-colors duration-100 {btnclass[
+			variant
+		]} {className ?? ''}"
 		{...props}
 		{onclick}
 	>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-	import { MediaQuery } from 'svelte/reactivity';
 	import Nav from './Nav.svelte';
-	const small = new MediaQuery('max-width: 512px');
-	$inspect(small.current);
 </script>
 
 <Nav />
