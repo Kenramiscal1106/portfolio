6ccb3afaa5b2c46fb16355576976601470a32dc5
Kenramiscal1106 <code.ramiscalken@gmail.com>
2025-09-25 00:19:55 +0800

    add temporary error boundary

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<h1 class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl font-bold">
+	{page.status}: {page.error && page.error.message}
+</h1>
